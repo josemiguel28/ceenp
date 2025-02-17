277c3e422b7599984aab708afa2f29313bcd362a
miguel ㅤ <miguel@MacBook-Air-de-miguel.local>
2025-02-16 19:44:23 -0600

    deployment

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -29,7 +29,7 @@
 
         <div class="text-center md:text-left">
             <p class="text-xl md:text-2xl md:font-semibold leading-tight">
-                Maestría en Neuropedagogía, cognición y didáctica (Testing)
+                Maestría en Neuropedagogía, cognición y didáctica
             </p>
         </div>
     </div>
